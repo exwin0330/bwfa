26de6aac15610f1e6c61d1fe9dae345bddc872db
Koki Ishii <ereven0330@gmail.com>
2024-09-09 12:52:32 +0900

    first commit

---
+++ src/dfa/code/Test.java
@@ -0,0 +1,15 @@
+package dfa.code;
+
+public class Test {
+	
+	public static void main(String[] args) {	
+		int i0 = 0;
+		int i1 = 1;
+
+		while (i0 < 10) {
+			i0 = i0 + i1;
+			i1 = i0;
+		}
+		return;
+	}
+}
---
+++ test/Test.java
@@ -0,0 +1,13 @@
+public class Test {
+	
+	public static void main(String[] args) {	
+		int i0 = 0;
+		int i1 = 1;
+
+		while (i0 < 10) {
+			i0 = i0 + i1;
+			i1 = i0;
+		}
+		return;
+	}
+}
